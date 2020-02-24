8b1c150f7b2861c80a76e39f00ee13412d9b8f5b
lalalashenle <50188235+lalalashenle@users.noreply.github.com>
2020-02-24 12:04:42 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+// Using characters to fill in the first heart shape
+// (x^2+y^2-1)^3 - x^2*y^3 = 0
+// y ~ (-1.1 , 1.3 )
+// x ~ (-1.2 , 1.2 )
+int main() {
+
+    string s = "I love you!";
+    for( float y = 1.3 ; y >= -1.1 ; y -= 0.06 ){
+        int index = 0;
+        for( float x = -1.2 ; x <= 1.2 ; x += 0.025 )
+            if( pow((x*x+y*y-1.0),3) - x*x*y*y*y <= 0.0 )
+                cout<<s[(index++)%s.size()];
+            else
+                cout<<' ';
+        cout<<endl;
+    }
+
+    return 0;
+}
